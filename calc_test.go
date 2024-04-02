6b48ef55e6ff4f5db2144b053d8614983b504789
jeffy <cjh6393@outlook.com>
2024-04-02 20:09:34 +0800

    1. add test case

--- calc_test.go
+++ calc_test.go
@@ -35,16 +35,30 @@ func TestSubtract(t *testing.T) {
 
 func TestDivide(t *testing.T) {
 	calc := newCalculator(test.NewApp())
-	calc.app.Preferences().SetFloat(PrecisionPref, 1)
-	calc.loadPreferences()
 	calc.loadUI()
 
-	test.Tap(calc.buttons["3"])
+	test.Tap(calc.buttons["4"])
 	test.Tap(calc.buttons["/"])
 	test.Tap(calc.buttons["2"])
 	test.Tap(calc.buttons["="])
 
-	assert.Equal(t, "1.5", calc.output.Text)
+	assert.Equal(t, "2", calc.output.Text)
+}
+
+func TestDividePrecision(t *testing.T) {
+	calc := newCalculator(test.NewApp())
+	calc.app.Preferences().SetFloat(PrecisionPref, 2)
+	calc.loadPreferences()
+	calc.loadUI()
+
+	test.Tap(calc.buttons["3"])
+	test.Tap(calc.buttons["."])
+	test.Tap(calc.buttons["5"])
+	test.Tap(calc.buttons["/"])
+	test.Tap(calc.buttons["2"])
+	test.Tap(calc.buttons["="])
+
+	assert.Equal(t, "1.75", calc.output.Text)
 }
 
 func TestMultiply(t *testing.T) {
@@ -202,4 +216,73 @@ func TestShortcuts(t *testing.T) {
 	clipboard.SetContent("not a valid number")
 	calc.onPasteShortcut(&fyne.ShortcutPaste{Clipboard: clipboard})
 	assert.Equal(t, "850", calc.output.Text)
+}
+
+func TestCalculationProcess(t *testing.T) {
+	calc := newCalculator(test.NewApp())
+	calc.loadUI()
+
+	test.Tap(calc.buttons["9"])
+	test.Tap(calc.buttons["."])
+	test.Tap(calc.buttons["2"])
+	test.Tap(calc.buttons["*"])
+	test.Tap(calc.buttons["3"])
+	test.Tap(calc.buttons["="])
+
+	assert.Equal(t, "9.2*3", calc.process.Text)
+
+	test.Tap(calc.buttons["C"])
+	assert.Equal(t, "", calc.process.Text)
+}
+
+func TestHistory(t *testing.T) {
+	calc := newCalculator(test.NewApp())
+	calc.loadUI()
+
+	test.Tap(calc.buttons["1"])
+	test.Tap(calc.buttons["+"])
+	test.Tap(calc.buttons["2"])
+	test.Tap(calc.buttons["="])
+	assert.Contains(t, calc.historyText.Text(), "1+2 = 3")
+
+	test.Tap(calc.buttons["C"])
+	test.Tap(calc.buttons["1"])
+	test.Tap(calc.buttons["*"])
+	test.Tap(calc.buttons["2"])
+	test.Tap(calc.buttons["="])
+	assert.Contains(t, calc.historyText.Text(), "1*2 = 2")
+
+	for _, item := range calc.menu().Items {
+		if item.Label == "Settings" {
+			for _, settingItem := range item.Items {
+				if settingItem.Label == "Show History" {
+					settingItem.Action()
+					assert.Equal(t, calc.isHistoryWindowOpen, true)
+				} else if settingItem.Label == "Clear History" {
+					settingItem.Action()
+					assert.Equal(t, "", calc.historyText.Text())
+				}
+			}
+			break
+		}
+	}
+}
+
+func TestPrecision(t *testing.T) {
+	calc := newCalculator(test.NewApp())
+	calc.loadUI()
+
+	for _, item := range calc.menu().Items {
+		if item.Label == "Settings" {
+			for _, settingItem := range item.Items {
+				if settingItem.Label == "Precision" {
+					for i, m := range settingItem.ChildMenu.Items {
+						m.Action()
+						assert.Equal(t, calc.precision, i)
+					}
+				}
+			}
+			break
+		}
+	}
 }
--- const.go
+++ const.go
@@ -1,7 +1,8 @@
 package main
 
+// preferences key
 const (
-	PrecisionPref   = "precision"
-	ShowHistoryPref = "historyWindowOpen"
-	HistoryTextPref = "historyText"
+	PrecisionPref   = "precision"         // the number of digits after the decimal
+	ShowHistoryPref = "historyWindowOpen" // whether the history window is open
+	HistoryTextPref = "historyText"       // the text in the history window
 )
